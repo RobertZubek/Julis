f0ebb387eb89d7d68164b30990f32514c48f8e76
Robert Zubek <robert@Roberts-MacBook-Air.local>
2024-01-04 19:17:58 +0100

    motors connected

--- Julis/Julis.ino
+++ Julis/Julis.ino
@@ -2,6 +2,8 @@
 #include <AFMotor.h>
 #include <Servo.h>
 
+#define rtDelay(v) vTaskDelay(v/15)
+
 #define servoPIN 9
 #define motor1PIN 1
 #define motor2PIN 2
@@ -9,13 +11,42 @@
 #define motor4PIN 4
 
 Servo servo;
-AF_DCMotor motor1(motor1PIN); 
-AF_DCMotor motor2(motor2PIN); 
-AF_DCMotor motor3(motor3PIN); 
-AF_DCMotor motor4(motor4PIN); 
+AF_DCMotor motor1(motor1PIN); //top right
+AF_DCMotor motor2(motor2PIN); //top left
+AF_DCMotor motor3(motor3PIN); //back left
+AF_DCMotor motor4(motor4PIN); //back right
 
 void setup() {
   servo.attach(servoPIN);
+  xTaskCreate(run, "run", 128, NULL, 1, NULL);
+  xTaskCreate(guard, "guard", 128, NULL, 1, NULL);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  for(i=0;i<=255;i++)
+  {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i);
+  }
+}
+void run(void*){
+
 }
 
+
+void guard(void*){
+
+}
+
+
+
 void loop() {}
+
+
+
+
+
+
