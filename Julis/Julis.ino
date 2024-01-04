b839d84e13cdadf311e528f27e09edb3c2aaa9be
Robert Zubek <robert@Roberts-MacBook-Air.local>
2024-01-04 19:39:30 +0100

    rtos and semaphore steup

--- Julis/Julis.ino
+++ Julis/Julis.ino
@@ -1,6 +1,8 @@
 #include <Arduino_FreeRTOS.h>
+#include "semphr.h"
 #include <AFMotor.h>
 #include <Servo.h>
+
 
 #define rtDelay(v) vTaskDelay(v/15)
 
@@ -10,34 +12,48 @@
 #define motor3PIN 3
 #define motor4PIN 4
 
+SemaphoreHandle_t binarySemaphore;
+
 Servo servo;
 AF_DCMotor motor1(motor1PIN); //top right
 AF_DCMotor motor2(motor2PIN); //top left
 AF_DCMotor motor3(motor3PIN); //back left
 AF_DCMotor motor4(motor4PIN); //back right
 
+int speed;
+
 void setup() {
+  
   servo.attach(servoPIN);
   xTaskCreate(run, "run", 128, NULL, 1, NULL);
   xTaskCreate(guard, "guard", 128, NULL, 1, NULL);
+  binarySemaphore = xSemaphoreCreateBinary();
+  xSemaphoreGive(binarySemaphore);
   motor1.run(FORWARD);
   motor2.run(FORWARD);
   motor3.run(FORWARD);
   motor4.run(FORWARD);
-  for(i=0;i<=255;i++)
+  for(speed=0;speed=255;speed++)
   {
-    motor1.setSpeed(i);
-    motor2.setSpeed(i);
-    motor3.setSpeed(i);
-    motor4.setSpeed(i);
+    motor1.setSpeed(speed);
+    motor2.setSpeed(speed);
+    motor3.setSpeed(speed);
+    motor4.setSpeed(speed);
   }
 }
 void run(void*){
-
+  speed=255;
+  xSemaphoreTake(binarySemaphore, portMAX_DELAY);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  xSemaphoreGive(binarySemaphore);
 }
 
 
 void guard(void*){
+  
 
 }
 
