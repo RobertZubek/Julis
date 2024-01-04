eaad18721c1592009bb00b0bdb02ca08e3c83be1
Robert Zubek <robert@Roberts-MacBook-Air.local>
2024-01-04 18:45:30 +0100

    servo attached

--- Julis/Julis.ino
+++ Julis/Julis.ino
@@ -1,9 +1,14 @@
+#include <Arduino_FreeRTOS.h>
+//#include <AFMotor.h>
+#include <Servo.h>
+#define servoPIN 9
+
+Servo servo; 
+
+
+
 void setup() {
-  
-
+  servo.attach(servoPIN);
 }
 
-void loop() {
-  
-
-}
+void loop() {}
