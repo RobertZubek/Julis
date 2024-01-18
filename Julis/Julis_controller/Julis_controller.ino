90872a29dd24d863becb8d94a5436c0838d877f9
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-01-18 22:56:53 +0100

    controller pinout

---
+++ Julis/Julis_controller/Julis_controller.ino
@@ -0,0 +1,17 @@
+#define upPIN 2//D4
+#define downPIN 0//D3
+#define leftPIN 5//D1
+#define rightPIN 15//D8
+
+void setup() {
+  pinMode(upPIN, INPUT_PULLUP);
+  pinMode(downPIN, INPUT_PULLUP);
+  pinMode(leftPIN, INPUT_PULLUP);
+  pinMode(rightPIN, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
