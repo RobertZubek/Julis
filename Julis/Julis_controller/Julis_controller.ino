a3a0f0dd764d9338186445fbe318959dc830c9cc
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-01-18 23:53:11 +0100

    sending data from controller

--- Julis/Julis_controller/Julis_controller.ino
+++ Julis/Julis_controller/Julis_controller.ino
@@ -1,9 +1,17 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+
 #define upPIN 2//D4
 #define downPIN 0//D3
 #define leftPIN 5//D1
 #define rightPIN 15//D8
+#define transPIN 14//D5
+
+uint8_t msg;
+RH_ASK driver;
 
 void setup() {
+
   pinMode(upPIN, INPUT_PULLUP);
   pinMode(downPIN, INPUT_PULLUP);
   pinMode(leftPIN, INPUT_PULLUP);
@@ -12,6 +20,16 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if(digitalRead(upPIN)==LOW) msg = 1;
+  else if(digitalRead(downPIN)==LOW) msg = 2;
+  else if(digitalRead(leftPIN)==LOW) msg = 3;
+  else if(digitalRead(rightPIN)==LOW) msg = 4;
+  else msg = 5;
+
+  driver.send((uint8_t *)msg, 8);
+  driver.waitPacketSent();
+  delay(1000);
+
 
 }
