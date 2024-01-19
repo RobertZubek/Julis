be26aa31c18a0adb44bbc5f56d7a684d8b3b3178
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-01-19 23:28:33 +0100

    controller buttons tested, D8 pulled to gnd, change to d7

--- Julis/Julis_controller/Julis_controller.ino
+++ Julis/Julis_controller/Julis_controller.ino
@@ -4,18 +4,22 @@
 #define upPIN 2//D4
 #define downPIN 0//D3
 #define leftPIN 5//D1
-#define rightPIN 15//D8
-#define transPIN 14//D5
+#define rightPIN 14//D7!!!!!!!!!!!!!!!!!!!do lutowanska
+#define transPIN 13//D5
 
-uint8_t msg;
-RH_ASK driver;
+int msg;
+//RH_ASK driver;
 
 void setup() {
 
+  Serial.begin(9600);
   pinMode(upPIN, INPUT_PULLUP);
   pinMode(downPIN, INPUT_PULLUP);
   pinMode(leftPIN, INPUT_PULLUP);
   pinMode(rightPIN, INPUT_PULLUP);
+//  if (!driver.init()) {
+//    Serial.println("RF driver init failed");
+//  }
 
 }
 
@@ -27,8 +31,9 @@ void loop() {
   else if(digitalRead(rightPIN)==LOW) msg = 4;
   else msg = 5;
 
-  driver.send((uint8_t *)msg, 8);
-  driver.waitPacketSent();
+  Serial.println(msg);
+ // driver.send(&msg, sizeof(msg));
+  //driver.waitPacketSent();
   delay(1000);
 
 
--- Julis/Julis_receive_controll/Julis_receive_controll.ino
+++ Julis/Julis_receive_controll/Julis_receive_controll.ino
@@ -1,4 +1,4 @@
-#include <AFMotor.h>
+//#include <AFMotor.h>
 #include <RH_ASK.h>
 #include <SPI.h>
 
@@ -8,17 +8,29 @@
 #define motor4PIN 4
 #define recPIN 16
 
-AF_DCMotor motor1(motor1PIN); //top right
+/*AF_DCMotor motor1(motor1PIN); //top right
 AF_DCMotor motor2(motor2PIN); //top left
 AF_DCMotor motor3(motor3PIN); //back left
-AF_DCMotor motor4(motor4PIN); //back right
+AF_DCMotor motor4(motor4PIN); //back right*/
+
+RH_ASK driver;
 
 void setup() {
-  
+  if (!driver.init()) {
+    Serial.println("RF driver init failed");
+  }
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint8_t buf[12];
+  uint8_t buflen = sizeof(buf);
+  
+  if (driver.recv(buf, &buflen)) // Non-blocking
+  {
+    int i;
+    Serial.println((char*)buf);
+  }
 
 }
