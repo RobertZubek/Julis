a85081ab66147e910578b69a8ab87321d5127590
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-02-03 19:54:58 +0100

    finally a working zommunication

--- Julis/Julis_controller/Julis_controller1/Julis_controller1.ino
+++ Julis/Julis_controller/Julis_controller1/Julis_controller1.ino
@@ -1,18 +1,21 @@
 #include <RCSwitch.h>
-#define transPIN 13 //D5
 
 RCSwitch mySwitch = RCSwitch();
 
 void setup() {
-  Serial.begin(9600);
-  mySwitch.enableTransmit(transPIN);
 
+  Serial.begin(9600);
+  
+  // Transmitter is connected to Arduino Pin #10  
+  mySwitch.enableTransmit(14);
 }
 
 void loop() {
-  mySwitch.send(1394007, 24);
-  delay(1000);
-  mySwitch.send(1394006, 24);
-  delay(1000);
+
+  
+  mySwitch.send("000000000001010100010001");
+  delay(500);  
+  mySwitch.send("000000000001010100010100");
+  delay(500);
 
 }
--- Julis/Julis_receive_controll/Julis_receive_controll.ino
+++ Julis/Julis_receive_controll/Julis_receive_controll.ino
@@ -1,36 +1,24 @@
-//#include <AFMotor.h>
-#include <RH_ASK.h>
-#include <SPI.h>
+#include <RCSwitch.h>
 
-#define motor1PIN 1
-#define motor2PIN 2
-#define motor3PIN 3
-#define motor4PIN 4
-#define recPIN 16
-
-/*AF_DCMotor motor1(motor1PIN); //top right
-AF_DCMotor motor2(motor2PIN); //top left
-AF_DCMotor motor3(motor3PIN); //back left
-AF_DCMotor motor4(motor4PIN); //back right*/
-
-RH_ASK driver;
+RCSwitch mySwitch = RCSwitch();
 
 void setup() {
-  if (!driver.init()) {
-    Serial.println("RF driver init failed");
-  }
   Serial.begin(9600);
-
+  mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
 }
 
 void loop() {
-  uint8_t buf[12];
-  uint8_t buflen = sizeof(buf);
-  
-  if (driver.recv(buf, &buflen)) // Non-blocking
-  {
-    int i;
-    Serial.println((char*)buf);
-  }
+  //if (mySwitch.available()) {
+    
+    Serial.print("Received ");
+    Serial.print( mySwitch.getReceivedValue() );
+    Serial.print(" / ");
+    Serial.print( mySwitch.getReceivedBitlength() );
+    Serial.print("bit ");
+    Serial.print("Protocol: ");
+    Serial.println( mySwitch.getReceivedProtocol() );
+    delay(100);
 
+    //mySwitch.resetAvailable();
+  //}
 }
