61f93f940ccb5c9214eae173340c5d1c490d23f5
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-02-03 21:45:18 +0100

    first driving version

--- Julis/Julis_receive_controll/Julis_receive_controll.ino
+++ Julis/Julis_receive_controll/Julis_receive_controll.ino
@@ -1,6 +1,17 @@
 #include <RCSwitch.h>
+#include <AFMotor.h>
+
+#define motor1PIN 1
+#define motor2PIN 2
+#define motor3PIN 3
+#define motor4PIN 4
 
 RCSwitch mySwitch = RCSwitch();
+
+AF_DCMotor motor1(motor1PIN); //top right
+AF_DCMotor motor2(motor2PIN); //top left
+AF_DCMotor motor3(motor3PIN); //back left
+AF_DCMotor motor4(motor4PIN); //back right
 
 void setup() {
   Serial.begin(9600);
@@ -8,17 +19,66 @@ void setup() {
 }
 
 void loop() {
-  //if (mySwitch.available()) {
-    /*
-    Serial.print("Received ");
-    Serial.print( mySwitch.getReceivedValue() );
-    Serial.print(" / ");
-    Serial.print( mySwitch.getReceivedBitlength() );
-    Serial.print("bit ");
-    Serial.print("Protocol: ");
-    Serial.println( mySwitch.getReceivedProtocol() );
-    delay(100);*/
+  
+  int dir=mySwitch.getReceivedValue();
 
-    //mySwitch.resetAvailable();
-  //}
+  int speed=255;
+  if(dir==5)
+  {
+    motor1.run(RELEASE);
+    motor2.run(RELEASE);
+    motor3.run(RELEASE);
+    motor4.run(RELEASE);
+  }
+  else if(dir==1)
+  {
+    motor1.setSpeed(speed);
+    motor2.setSpeed(speed);
+    motor3.setSpeed(speed);
+    motor4.setSpeed(speed);
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
+  }
+  else if(dir==2)
+  {
+    motor1.setSpeed(speed);
+    motor2.setSpeed(speed);
+    motor3.setSpeed(speed);
+    motor4.setSpeed(speed);
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);
+  }
+  else if(dir==3)
+  {
+    motor1.setSpeed(speed);
+    motor2.setSpeed(speed);
+    motor3.setSpeed(speed);
+    motor4.setSpeed(speed);
+    motor1.run(FORWARD);
+    motor2.run(BACKWARD);
+    motor3.run(BACKWARD);
+    motor4.run(FORWARD);
+  }
+  else if(dir==4)
+  {
+    motor1.setSpeed(speed);
+    motor2.setSpeed(speed);
+    motor3.setSpeed(speed);
+    motor4.setSpeed(speed);
+    motor1.run(BACKWARD);
+    motor2.run(FORWARD);
+    motor3.run(FORWARD);
+    motor4.run(BACKWARD);
+  }
+  else
+  {
+    motor1.run(RELEASE);
+    motor2.run(RELEASE);
+    motor3.run(RELEASE);
+    motor4.run(RELEASE);
+  }
 }
